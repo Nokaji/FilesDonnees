f30ba09caac9c9b88a9902170c17675e955d5c60
Nokaji <nokaji.dev@gmail.com>
2024-01-21 19:07:55 +0100

    update

--- modules/logging.js
+++ modules/logging.js
@@ -8,8 +8,9 @@ const fs = require("fs-extra");
  */
 
 function logging(string, ...any){
+    const date = new Date();
     if(!fs.existsSync("./log.txt")) fs.createFile("./log.txt");
-    fs.appendFile("./log.txt", [string + any + "\n"].toString());
+    fs.appendFile("./log.txt", ["(" + date.toUTCString() + ")" + string + any + "\n"].toString());
 }
 
 module.exports = {logging};
