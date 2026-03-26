9ec770249523b055879ed9a5f075824c8f7beeca
Nokaji <nokaji.dev@gmail.com>
2026-03-26 13:59:47 +0100

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs-extra");
 const ConfigManager = require("./managers/ConfigManager");
-const {downloadFiles} = require("./modules/downloadFiles");
+const { downloadFiles } = require("./modules/downloadFiles");
 const path = require("path");
 const { engine } = require("express-handlebars");
 const getFolders = require("./modules/getFolders");
@@ -9,17 +9,23 @@ const { log } = require("console");
 const auth = require("basic-auth");
 const url = require("url");
 
-class APP{
-    app = express();
+class APP {
+  app = express();
 
-    constructor(){
-        this.app.engine('handlebars', engine({ defaultLayout: false, helpers:{
-            baseUrl: ConfigManager.APP.APP_URL
-        }}));
-        this.app.set('view engine', 'handlebars');
-        this.app.set('views', './views');
-        
-        this.initRouter();
+  constructor() {
+    this.app.engine(
+      "handlebars",
+      engine({
+        defaultLayout: false,
+        helpers: {
+          baseUrl: ConfigManager.APP.APP_URL,
+        },
+      }),
+    );
+    this.app.set("view engine", "handlebars");
+    this.app.set("views", "./views");
+
+    this.initRouter(); /*
         downloadFiles(ConfigManager.URL.NSI);
         downloadFiles(ConfigManager.URL.SNIR);
         downloadFiles(ConfigManager.URL.SNT);
@@ -29,79 +35,106 @@ class APP{
             downloadFiles(ConfigManager.URL.SNIR);
             setTimeout(() => {}, 1000);
             downloadFiles(ConfigManager.URL.SNT);
-        }, 1000*60*20);
+        }, 1000*60*20);*/
 
-        this.initServer();
-    }
+    this.initServer();
+  }
 
-    initRouter(){
-        this.app.use("/assets", express.static(path.join(__dirname, "assets")));
+  initRouter() {
+    this.app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-        this.app.use("/public", express.static(path.join(__dirname, "DATA")));
+    this.app.use("/public", express.static(path.join(__dirname, "DATA")));
 
-        this.app.set('case sensitive routing', false);
+    this.app.set("case sensitive routing", false);
 
-        const authenticate = (req, res, next) => {
-            const credentials = auth(req);
-        
-            if (!credentials || (credentials.name !== ConfigManager.APP.APP_USER || credentials.pass !== ConfigManager.APP.APP_PASSWORD) && (credentials.name !== ConfigManager.APP.APP_OTHER_USER || credentials.pass !== ConfigManager.APP.APP_OTHER_PASSWORD)) {
-                res.set('WWW-Authenticate', 'Basic realm="example"');
-                return res.status(401).send('Authentification requise.');
-            }
-        
-            return next();
-        };
+    const authenticate = (req, res, next) => {
+      const credentials = auth(req);
 
-        this.app.use("/Cours", authenticate);
+      if (
+        !credentials ||
+        ((credentials.name !== ConfigManager.APP.APP_USER ||
+          credentials.pass !== ConfigManager.APP.APP_PASSWORD) &&
+          (credentials.name !== ConfigManager.APP.APP_OTHER_USER ||
+            credentials.pass !== ConfigManager.APP.APP_OTHER_PASSWORD))
+      ) {
+        res.set("WWW-Authenticate", 'Basic realm="example"');
+        return res.status(401).send("Authentification requise.");
+      }
 
-        this.app.get('/*', async (req, res, next) => {
-            try {
-                req.requestedFolder = req.params[0];
-                const urlActuelle = "/" + req.requestedFolder;
-                const previousUrl = urlActuelle.split("/").slice(0, -1).join("/");
-                const currentFolder = path.join("DATA", req.requestedFolder);
-                const navigationFolder = path.join("/", req.requestedFolder);// Var Navigate
-                let isReturn = false;
+      return next();
+    };
 
-                if(previousUrl !== "" || req.requestedFolder !== ""){
-                    isReturn = true;
-                }
+    this.app.use("/Cours", authenticate);
 
-                const tree = await JSON.parse(await getFolders.jsonTree(currentFolder));
+    this.app.get("/*", async (req, res, next) => {
+      try {
+        req.requestedFolder = req.params[0];
+        const urlActuelle = "/" + req.requestedFolder;
+        const previousUrl = urlActuelle.split("/").slice(0, -1).join("/");
+        const currentFolder = path.join("DATA", req.requestedFolder);
+        const navigationFolder = path.join("/", req.requestedFolder); // Var Navigate
+        let isReturn = false;
 
-                const treeWithUrls = Object.entries(tree).map(([name, value]) => {
+        if (previousUrl !== "" || req.requestedFolder !== "") {
+          isReturn = true;
+        }
 
-                    const isFile = value === null;
-                    const urlPath = isFile ? ConfigManager.APP.APP_URL + "/public/" + req.requestedFolder + "/" + encodeURIComponent(name) : path.join("/", req.requestedFolder, encodeURIComponent(name));
-                    const fullUrl = ConfigManager.APP.APP_URL + urlPath;
-                    let isLink = false;
+        const tree = await JSON.parse(await getFolders.jsonTree(currentFolder));
 
-                    if(value !== null && typeof value === "string"){
-                        if(value.startsWith("https://")) isLink = true;
-                    }
+        const treeWithUrls = Object.entries(tree).map(([name, value]) => {
+          const isFile = value === null;
+          const urlPath = isFile
+            ? ConfigManager.APP.APP_URL +
+              "/public/" +
+              req.requestedFolder +
+              "/" +
+              encodeURIComponent(name)
+            : path.join("/", req.requestedFolder, encodeURIComponent(name));
+          const fullUrl = ConfigManager.APP.APP_URL + urlPath;
+          let isLink = false;
 
-                    return {
-                        name,
-                        url: fullUrl,
-                        urlPath,
-                        isFile,
-                        isLink,
-                        link: value
-                    };
-                });
-        
-                res.render("index", { tree: treeWithUrls, previousUrl: ConfigManager.APP.APP_URL + previousUrl , isReturn, currentFolder: navigationFolder});
-            } catch (error) {
-                console.error('Erreur lors de la génération de l\'arborescence JSON :', error);
-                res.status(500).send('Une erreur est survenue lors de la récupération de l\'arborescence.');
-            }
+          if (value !== null && typeof value === "string") {
+            if (value.startsWith("https://")) isLink = true;
+          }
+
+          return {
+            name,
+            url: fullUrl,
+            urlPath,
+            isFile,
+            isLink,
+            link: value,
+          };
         });
-    }
 
-    initServer(){
-        this.app.listen(ConfigManager.APP.APP_PORT, ConfigManager.APP.APP_HOST, () => {
-            console.log("Starting Server...");
+        res.render("index", {
+          tree: treeWithUrls,
+          previousUrl: ConfigManager.APP.APP_URL + previousUrl,
+          isReturn,
+          currentFolder: navigationFolder,
         });
-    }
+      } catch (error) {
+        console.error(
+          "Erreur lors de la génération de l'arborescence JSON :",
+          error,
+        );
+        res
+          .status(500)
+          .send(
+            "Une erreur est survenue lors de la récupération de l'arborescence.",
+          );
+      }
+    });
+  }
+
+  initServer() {
+    this.app.listen(
+      ConfigManager.APP.APP_PORT,
+      ConfigManager.APP.APP_HOST,
+      () => {
+        console.log("Starting Server...");
+      },
+    );
+  }
 }
 new APP();
